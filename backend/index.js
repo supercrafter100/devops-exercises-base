b6db7ef224dc4290f6c5505d1f8c23faf1f9aff9
Dries Van Hansewijck <dries.vanhansewijck@ap.be>
2024-08-27 09:00:49 +0200

    Update backend poort

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000;
+const port = 4000;
 const verify = require('./verify');
 
 const useMongoDB = process.env.STORAGE && process.env.STORAGE === 'mongodb';
