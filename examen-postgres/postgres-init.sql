d0a19a1d7657603e55f845f851f1f70ea5134e5f
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2025-01-21 17:03:26 +0100

    forgot to push this

--- examen-postgres/postgres-init.sql
+++ examen-postgres/postgres-init.sql
@@ -1,10 +1,10 @@
 CREATE table blogs (
-    id serial primary key,
+    id text primary key,
     title varchar(255),
     content text
 );
 
-INSERT INTO blogs (title, content) VALUES ('Eerste blog met PostgreSQL!', 'Je hebt nu je eerste blog met PostgreSQL aangemaakt!');
-INSERT INTO blogs (title, content) VALUES ('Super coole veggie info', 'Vandaag heb ik een super coole blog geschreven over vegetarisch eten!');
-INSERT INTO blogs (title, content) VALUES ('Mijn eerste blog', 'Dit is mijn eerste blog ooit!');
-INSERT INTO blogs (title, content) VALUES ('Wat ik vanavond ga eten', 'Ik ga vanavond lekker koken, ik heb er zin in!');
+INSERT INTO blogs (id, title, content) VALUES ('a', 'Eerste blog met PostgreSQL!', 'Je hebt nu je eerste blog met PostgreSQL aangemaakt!');
+INSERT INTO blogs (id, title, content) VALUES ('b', 'Super coole veggie info', 'Vandaag heb ik een super coole blog geschreven over vegetarisch eten!');
+INSERT INTO blogs (id, title, content) VALUES ('c', 'Mijn eerste blog', 'Dit is mijn eerste blog ooit!');
+INSERT INTO blogs (id, title, content) VALUES ('d', 'Wat ik vanavond ga eten', 'Ik ga vanavond lekker koken, ik heb er zin in!');
